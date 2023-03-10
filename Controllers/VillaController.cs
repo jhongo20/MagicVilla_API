5ec439c1fe95cf8b1bd43e3e5cbb18fa3cbd59ec
Jhon Jairo Pérez <jhon16_39@hotmail.com>
2023-03-10 08:47:29 -0500

    Primer Api de prueba funcionando sin base de datos, trae los datos de VillaDto

---
+++ Controllers/VillaController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+using MagicVilla_API.Datos;
+
+namespace MagicVilla_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDto> GetVillas()
+        {
+            return VillaStore.villaLista;
+        }
+
+        [HttpGet("id:int")]
+        public VillaDto GetVilla(int id)
+        {
+            return VillaStore.villaLista.FirstOrDefault(v => v.Id==id);
+        }
+
+    }
+}
